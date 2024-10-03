4fac881830b848014bc286bce9dd28f5dfefdc38
Farhana Alam Japse <farhanjapz@gmail.com>
2024-10-03 14:28:01 +0600

    leaflet used

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    //console.log(position);
+
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`); //https://www.google.com/maps/@22.3350468,91.8062497
+
+    const coords = [latitude, longitude];
+
+    var map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
+    alert('could not get your position');
+  }
+); //1st callback is cur position,2nd callback is the err callback
